347cf81752934e70bf40773bee476a62793a6766
YASH RAJ <99674512+Y4shr4j@users.noreply.github.com>
2023-01-04 09:07:18 +0530

    Update About.js
    Signed-off-by: YASH RAJ <99674512+Y4shr4j@users.noreply.github.com>

--- src/component/About.js
+++ src/component/About.js
@@ -42,7 +42,7 @@ export default function About() {
         <div className="accordion-item" style={mystyle}>
             <h2 className="accordion-header" id="headingOne">
             <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" style={mystyle}>
-                Accordion Item #1
+                Acknowledgement
             </button>
             </h2>
             <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
@@ -54,7 +54,7 @@ export default function About() {
         <div className="accordion-item" style={mystyle}>
             <h2 className="accordion-header" id="headingTwo" >
             <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo" style={mystyle}>
-                Accordion Item #2
+                Contuct Us
             </button>
             </h2>
             <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
@@ -65,8 +65,8 @@ export default function About() {
         </div>
         <div className="accordion-item" style={mystyle}>
             <h2 className="accordion-header" id="headingThree">
-            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree" style={mystyle}>
-                Accordion Item #3
+            <button className="accordion-button collapsed" style={mystyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree" >
+                How to use
             </button>
             </h2>
             <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
